bd96c378752c7dff21e2a7a36ec021101b8a0a4f
vikalejo <vikalejo@gmail.com>
2015-07-27 00:31:12 +0200

    Add files and sum_path

--- lib/developer_challenge/path_sum.rb
+++ lib/developer_challenge/path_sum.rb
@@ -1,9 +1,29 @@
 module DeveloperChallenge
   class PathSum
     def minimum_path_sum(file)
-      puts 'yoyo'
+      read_structure(file) unless !File.exist?("data/#{file}")
+      abort "The file does not exist"
     end
 
+    def read_structure(file)
+      line_num=0
+      data_triangle = File.read("data/#{file}")
+      triangle = data_triangle.each_line.map do |line|
+        line.split.map(&:to_i)
+      end
+      sum_path(triangle)
+      exit
+    end
+
+    def sum_path(triangle)
+      puts triangle.inject([]) { |res,x|
+        print " res es: #{res}"
+        print " x es: #{x}"
+        minima = [0, *res, 0].each_cons(2).map(&:min)
+        puts " minima es: #{minima}"
+        x.zip(minima).map{ |a,b| a+b }
+      }
+    end
   end
 end
 
